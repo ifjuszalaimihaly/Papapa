df871229fa9967271c6fc119385ac39d84ae924e
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2017-10-23 18:21:32 +0200

    begin develop fest selection functionality

--- app/FestPredication.php
+++ app/FestPredication.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class FestPredication extends Model
 {
-    //
+    protected $table='fest_predication';
+
+    public function fest()
+    {
+        return $this->belongsTo(Fest::class);
+    }
+
+    public function festDatesInYear(int $year)
+    {
+        FestPredication::whereYear('written_at',$year)->get();
+
+    }
 }
