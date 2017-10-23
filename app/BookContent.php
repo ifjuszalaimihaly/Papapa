9e6daff9c2a096213427fd03576415c6bf8ddb13
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2017-10-23 14:07:40 +0200

    added chapter and versicle count queries to BookContent

--- app/BookContent.php
+++ app/BookContent.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BookContent extends Model
 {
+    public function maxChapter(int $book_id, int $bible_translation_id)
+    {
+        return BookContent::where([['book_id',$book_id],['bible_translation_id',$bible_translation_id]])->groupBy('book_id','bible_translation_id')->max('chapter');
+    }
 
+    public function maxVersicle(int $book_id, int $bible_translation_id, int $chapter)
+    {
+        return BookContent::where([['book_id',$book_id],['bible_translation_id',$bible_translation_id],['chapter', $chapter]])->groupBy('book_id','bible_translation_id', 'chapter')->max('versicle');
+    }
 }
